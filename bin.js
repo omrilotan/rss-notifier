490f4f1735276fd9516643c26248bd65725a1999
omrilotan <omri@hamadgera.com>
2022-01-31 13:57:16 +0000

    Throw error when list is not available

--- bin.js
+++ bin.js
@@ -8,6 +8,10 @@ const formatter = require('./lib/formatter')
 const [, , ...argv] = process.argv
 const trim = i => i.trim()
 async function readYaml (file) {
+  if (!file) {
+    return {}
+  }
+
   const content = await readFile(file, 'utf8')
   return load(content)
 }
@@ -20,31 +24,10 @@ async function start (argv) {
   const config = args.config || process.env.CONFIG
 
   options.logLevel = args.logLevel || process.env.LOG_LEVEL || 'warn'
+  options.logFormat = args.logFormat || process.env.LOG_FORMAT || 'plain'
+
   const logger = levelheaded({ minimal: options.logLevel })
   const _logger = formatter(logger, options.logFormat)
-
-  const configData = config
-    ? await readYaml(config)
-    : {}
-
-  // Priority: CLI > YAML > ENV
-  options.logLevel = args.logLevel || configData.logLevel || process.env.LOG_LEVEL || 'warn'
-  options.logFormat = args.logFormat || configData.logFormat || process.env.LOG_FORMAT || 'plain'
-  options.interval = args.interval || configData.interval || process.env.INTERVAL
-  options.webhook = args.webhook || configData.webhook || process.env.WEBHOOK
-  options.channel = args.channel || configData.channel || process.env.CHANNEL
-  options.emoji = args.emoji || configData.emoji || process.env.EMOJI
-  options.dryRun = Boolean(args.dryRun ?? configData.dryRun ?? process.env.DRY_RUN)
-  options.list = (() => {
-    const feed = args.feed || args.feeds || configData.feed || configData.feeds || process.env.FEED || process.env.FEEDS
-    if (Array.isArray(feed)) {
-      return feed
-    }
-    if (typeof feed === 'string') {
-      return feed.split(',').map(trim)
-    }
-    return []
-  })()
 
   process.on(
     'unhandledRejection',
@@ -64,8 +47,39 @@ async function start (argv) {
     )
   )
 
+  try {
+    const configData = await readYaml(config)
+
+    // Priority: CLI > YAML > ENV
+    options.interval = args.interval || configData.interval || process.env.INTERVAL
+    options.webhook = args.webhook || configData.webhook || process.env.WEBHOOK
+    options.channel = args.channel || configData.channel || process.env.CHANNEL
+    options.emoji = args.emoji || configData.emoji || process.env.EMOJI
+    options.dryRun = Boolean(args.dryRun ?? configData.dryRun ?? process.env.DRY_RUN)
+    options.list = (() => {
+      const feed = args.feed || args.feeds || configData.feed || configData.feeds || process.env.FEED || process.env.FEEDS
+      if (Array.isArray(feed)) {
+        return feed
+      }
+      if (typeof feed === 'string') {
+        return feed.split(',').map(trim)
+      }
+      return []
+    })()
+  } catch (error) {
+    _logger.error(error)
+    process.exit(1)
+  }
+
+  if (!options.list) {
+    _logger.error('No feed specified', {
+      options: JSON.stringify(options)
+    })
+    process.exit(1)
+  }
+
   Promise.all(
-    options.list?.map(
+    options.list.map(
       (feed) => notifier({ ...options, logger, feed })
     )
   ).then(
